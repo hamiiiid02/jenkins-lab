5fe24f35fdb5713013b131eaffd7daa4992b2762
amineallali <amineallali9@gmail.com>
2025-10-16 17:59:11 +0100

    new file:   app.py 	new file:   requirements.txt 	new file:   templates/index.html 	new file:   test_app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html", message=None, sum_result=None)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form.get('username','').strip()
+    if name:
+        return render_template("index.html", message=f"Hello {name}, welcome to Jenkins CI/CD demo!", sum_result=None)
+    else:
+        return render_template("index.html", message="no name", sum_result=None), 404
+
+@app.route('/sum', methods=['POST'])
+def calculate_sum():
+    try:
+        a = float(request.form.get('a', 0))
+        b = float(request.form.get('b', 0))
+        result = a + b
+        return render_template("index.html", message=None, sum_result=result)
+    except ValueError:
+        return render_template("index.html", message=None, sum_result="Invalid input")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
